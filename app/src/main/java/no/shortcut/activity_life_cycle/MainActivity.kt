9cb7e6e43712905039df5c317290ad66fa1a091d
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-01-18 23:14:56 +0100

    hello world

---
+++ app/src/main/java/no/shortcut/activity_life_cycle/MainActivity.kt
@@ -0,0 +1,12 @@
+package no.shortcut.activity_life_cycle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/no/shortcut/activity_life_cycle/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package no.shortcut.activity_life_cycle
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
