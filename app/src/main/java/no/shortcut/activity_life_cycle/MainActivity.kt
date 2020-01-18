400ae2966faee540d6b72cbe7b98783a8600586d
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-01-18 23:20:38 +0100

    Adding logs

--- app/src/main/java/no/shortcut/activity_life_cycle/MainActivity.kt
+++ app/src/main/java/no/shortcut/activity_life_cycle/MainActivity.kt
@@ -1,6 +1,7 @@
 package no.shortcut.activity_life_cycle
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(this.javaClass.simpleName, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(this.javaClass.simpleName, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(this.javaClass.simpleName, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(this.javaClass.simpleName, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(this.javaClass.simpleName, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(this.javaClass.simpleName, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(this.javaClass.simpleName, "onRestart")
     }
 }
